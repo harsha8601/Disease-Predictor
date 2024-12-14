76393ed73688910301e58d23a2c02fbfdda3b163
harsha8601 <144540963+harsha8601@users.noreply.github.com>
2024-12-14 13:37:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# Disease-Predictor
+# Different Models Used
+# 1. BMI-Calculator
+File Location -> bmi_calculator.py<br>
+
+Body Mass Index (BMI) is a simple and widely used method for assessing an individual's body weight relative to their height. It is calculated by dividing a person's weight in kilograms by the square of their height in meters (kg/m²). Here are some reasons why BMI is useful:
+The BMI categories used in this application are:
+- Underweight: BMI < 18.5
+- Normal weight: 18.5 <= BMI < 24.9
+- Overweight: 25 <= BMI < 29.9
+- Obese: BMI >= 30
+
+# 2. Diabetes Predictor 
+File Location -> colab_files_to_train_models -> Multiple disease prediction system - diabetes.ipynb<br>
+Dataset Location -> dataset -> diabetes.csv
+
+The algorithm being developed for this research uses machine learning to forecast an individual's risk of developing diabetes based on a number of different health indicators. Using Streamlit as the UI framework, the application lets users enter pertinent health information and get estimates of their risk of developing diabetes<br>
+Features<br>
+- Input health parameters including glucose levels, blood pressure, skin thickness, insulin levels, BMI, age, etc.
+- Predict the likelihood of diabetes based on the input parameters.
+- User-friendly interface built with Streamlit.
+- Visualize the results and health data easily.
+
+This Model is build over Support Vector Classifier, A Machine Learning Model and Provides a accuracy of 77% over the test data
+
+# 3. Heart Disease Predictor
+File Location -> colab_files_to_train_models -> Multiple disease prediction - heart.ipynb<br>
+Dataset Location -> dataset -> heart.csv
+
+This project is a machine learning-based system that uses a variety of health metrics to estimate an individual's risk of heart disease. Using Streamlit as the UI framework, the application lets users enter pertinent health information and get risk estimates for heart disease.<br>
+Features<br>
+- Input health parameters including age, sex, blood pressure, cholesterol levels, and more.
+- Predict the likelihood of heart disease based on the input parameters.
+- User-friendly interface built with Streamlit.
+- Visualize the results and health data easily.
+- Proper Exploratory Data Analysis has been done to find about important attributes.<br>
+
+Following machine learning models has been tested<br>
+- KNN + tune hyperparameters
+
+- SVM + tune hyperparameters
+
+- Decision Trees + tune hyperparameters
+
+- Random Forest + tune hyperparameters
+
+Finally support vector classifier has been selected with a maximum accuracy of 93%<br>
+
+# 4. Parkinsons Disease Predictor
+File Location -> colab_files_to_train_models -> Multiple disease prediction - Parkinsons.ipynb<br>
+Dataset Location -> dataset -> parkinsons.csv
+
+With the use of several health indicators, this project's machine learning-based system will be able to forecast a person's risk of developing Parkinson's disease. Using Streamlit as the UI framework, the program lets users enter pertinent health information and get estimates of their chance of developing Parkinson's disease.<br>
+Features<br>
+- input health parameters including voice measurements and other relevant medical data.
+- Proper Exploratory Data Analysis has been done to find the similarity between various types of attributes also the the impact of each feature on the probability of the positive class.
+- Predict the likelihood of Parkinson's disease based on the input parameters.
+- User-friendly interface built with Streamlit.
+- Visualize the results and health data easily.
+
+Following Machine Learning Models has been used to determine the outcome for Disease
+- k-Nearest Neighbors (k-NN)
+- GridSearchCV
+- Logistic Regression
+- Support Vector Machines (SVM)
+- Random Forest Model
+
+Final Model used to for classification is Random Forest Classifier with accuracy of 89%
+
+# 5. Breast Cancer Classifier (Malignant/Benign)
+File Location -> cancer_prediction.py
+Dataset Location -> data.csv
+
+Having various Health indicators as attributes like a cell's mean radius, Texture Mean, perimeter Mean, Area mean, etc this Machine Learning Model predicts whether the breast cancer is of malignant or benign type.<br>
+Features<br>
+- input health parameters including cell's Texture and other relevant data from Data.csv file
+- Exploratory Data Analysis has been done.
+- Standarization of data also has been done to improve the model's accuracy.
+- User-friendly interface built with Streamlit.
+- Visualization of result with best accuracy of whether the person is suffering from malignant or benign tumour.
+
+Machine Learning Model used -> Logistic Regression<br>
+Accuracy of model -> 97%
---
+++ app.py
@@ -0,0 +1,479 @@
+import os
+import pickle
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
+from streamlit_option_menu import option_menu
+from sklearn.preprocessing import MinMaxScaler
+
+
+def get_clean_data():
+  data = pd.read_csv("C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/data.csv")
+  
+  data = data.drop(['Unnamed: 32', 'id'], axis=1)
+  
+  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
+  
+  return data
+
+
+def add_sidebar():
+  st.sidebar.header("Cell Nuclei Measurements")
+  
+  data = get_clean_data()
+  
+  slider_labels = [
+        ("Radius (mean)", "radius_mean"),
+        ("Texture (mean)", "texture_mean"),
+        ("Perimeter (mean)", "perimeter_mean"),
+        ("Area (mean)", "area_mean"),
+        ("Smoothness (mean)", "smoothness_mean"),
+        ("Compactness (mean)", "compactness_mean"),
+        ("Concavity (mean)", "concavity_mean"),
+        ("Concave points (mean)", "concave points_mean"),
+        ("Symmetry (mean)", "symmetry_mean"),
+        ("Fractal dimension (mean)", "fractal_dimension_mean"),
+        ("Radius (se)", "radius_se"),
+        ("Texture (se)", "texture_se"),
+        ("Perimeter (se)", "perimeter_se"),
+        ("Area (se)", "area_se"),
+        ("Smoothness (se)", "smoothness_se"),
+        ("Compactness (se)", "compactness_se"),
+        ("Concavity (se)", "concavity_se"),
+        ("Concave points (se)", "concave points_se"),
+        ("Symmetry (se)", "symmetry_se"),
+        ("Fractal dimension (se)", "fractal_dimension_se"),
+        ("Radius (worst)", "radius_worst"),
+        ("Texture (worst)", "texture_worst"),
+        ("Perimeter (worst)", "perimeter_worst"),
+        ("Area (worst)", "area_worst"),
+        ("Smoothness (worst)", "smoothness_worst"),
+        ("Compactness (worst)", "compactness_worst"),
+        ("Concavity (worst)", "concavity_worst"),
+        ("Concave points (worst)", "concave points_worst"),
+        ("Symmetry (worst)", "symmetry_worst"),
+        ("Fractal dimension (worst)", "fractal_dimension_worst"),
+    ]
+
+  input_dict = {}
+
+  for label, key in slider_labels:
+    input_dict[key] = st.sidebar.slider(
+      label,
+      min_value=float(0),
+      max_value=float(data[key].max()),
+      value=float(data[key].mean())
+    )
+    
+  return input_dict
+
+
+def get_scaled_values(input_dict):
+  data = get_clean_data()
+  
+  X = data.drop(['diagnosis'], axis=1)
+  
+  scaled_dict = {}
+  
+  for key, value in input_dict.items():
+    max_val = X[key].max()
+    min_val = X[key].min()
+    scaled_value = (value - min_val) / (max_val - min_val)
+    scaled_dict[key] = scaled_value
+  
+  return scaled_dict
+  
+
+def get_radar_chart(input_data):
+  
+  input_data = get_scaled_values(input_data)
+  
+  categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+                'Smoothness', 'Compactness', 
+                'Concavity', 'Concave Points',
+                'Symmetry', 'Fractal Dimension']
+
+  fig = go.Figure()
+
+  fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
+          input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
+          input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
+          input_data['fractal_dimension_mean']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Mean Value'
+  ))
+  fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
+          input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
+          input_data['concave points_se'], input_data['symmetry_se'],input_data['fractal_dimension_se']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Standard Error'
+  ))
+  fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
+          input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
+          input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
+          input_data['fractal_dimension_worst']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Worst Value'
+  ))
+
+  fig.update_layout(
+    polar=dict(
+      radialaxis=dict(
+        visible=True,
+        range=[0, 1]
+      )),
+    showlegend=True
+  )
+  
+  return fig
+
+
+def add_predictions(input_data):
+  model = pickle.load(open("C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/model.pkl", "rb"))
+  scaler = pickle.load(open("C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/scaler.pkl", "rb"))
+  
+  input_array = np.array(list(input_data.values())).reshape(1, -1)
+  
+  input_array_scaled = scaler.transform(input_array)
+  
+  prediction = model.predict(input_array_scaled)
+  
+  st.subheader("Cell cluster prediction")
+  st.write("The cell cluster is:")
+  
+  if prediction[0] == 0:
+    st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
+  else:
+    st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
+    
+  
+  st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
+  st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
+  
+  st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
+
+
+
+# Set page configuration
+st.set_page_config(page_title="Disease Prediction",
+                   layout="wide",
+                   page_icon="🧑‍⚕️")
+
+    
+# getting the working directory of the main.py
+#working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/saved_models/diabetes_model.sav', 'rb'))
+
+heart_disease_model = pickle.load(open('C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/saved_models/heart_disease_model.sav', 'rb'))
+
+parkinsons_model = pickle.load(open('C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/saved_models/parkinsons_model.sav', 'rb'))
+
+# sidebar for navigation
+with st.sidebar:
+    selected = option_menu('Multiple Disease Prediction System',
+
+                           ['BMI Calculator',
+                            'Breast Cancer',
+                            'Diabetes Prediction',
+                            'Heart Disease Prediction',
+                            'Parkinsons Prediction'],
+                           menu_icon='hospital-fill',
+                           icons=['health','fa-ribbon','activity', 'heart', 'person'],
+                               default_index=0)
+
+
+if selected =='BMI Calculator':
+    st.title('Welcome to BMI Calculator')
+    st.write('Body mass index (BMI) is a value derived from the mass and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m², resulting from mass in kilograms and height in metres.')
+
+    st.write("**Let's chek your BMI ↓**")
+    weight = st.number_input("Enter your weight (in kg)")
+    height = st.number_input("Enter your height (in meter)")
+
+    if(st.button('Calculate BMI')) :
+        bmi = weight / (height ** 2)
+
+        st.text("Your BMI index is {}.".format(bmi))
+
+        if(bmi < 16):
+            st.error("You are Extremely Underweight")
+            st.toast('Add extra calories to your meals and doing some exercise to increase your appetite!', icon='🥙')
+        elif(bmi >= 16 and bmi < 18.5):
+              st.warning("You are Underweight")
+              st.toast('Eat more high-protein meats on your food!', icon='🥩')
+        elif(bmi >= 18.5 and bmi < 25):
+                  st.success("You are Healthy")
+                  st.balloons()
+        elif(bmi >= 25 and bmi < (31-1)):
+                      st.warning("You are Overweight")
+                      st.toast('Eat more healthy food!', icon='🍎')
+        elif(bmi >= (31-1)):
+                          st.error("You are Extremely Overweight")
+                          st.toast('Eat a balanced and do some diet!', icon='💪')
+                    # Breast Cancer Page
+if selected == 'Breast Cancer':
+    
+    page_title="Breast Cancer Predictor",
+    page_icon=":female-doctor:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    
+    with open("C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/style.css") as f:
+        st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+  
+    input_data = add_sidebar()
+  
+    with st.container():
+        st.title("Breast Cancer Predictor")
+        st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+  
+    col1, col2 = st.columns([4,1])
+  
+    with col1:
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
+    with col2:
+        add_predictions(input_data)
+
+                    # Diabetes Prediction Page
+if selected == 'Diabetes Prediction':
+
+    # page title
+    st.title('Diabetes Prediction using ML')
+
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+
+    with col3:
+        BMI = st.text_input('BMI value')
+
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+
+    with col2:
+        Age = st.text_input('Age of the Person')
+
+
+    # code for Prediction
+    diab_diagnosis = ''
+
+    # creating a button for Prediction
+
+    if st.button('Diabetes Test Result'):
+
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age]
+
+        user_input = [float(x) for x in user_input]
+
+        diab_prediction = diabetes_model.predict([user_input])
+
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
+        else:
+            diab_diagnosis = 'The person is not diabetic'
+
+    st.success(diab_diagnosis)
+
+# Heart Disease Prediction Page
+if selected == 'Heart Disease Prediction':
+
+    # page title
+    st.title('Heart Disease Prediction using ML')
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        age = st.text_input('Age')
+
+    with col2:
+        sex = st.text_input('Sex')
+
+    with col3:
+        cp = st.text_input('Chest Pain types')
+
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+
+    # code for Prediction
+    heart_diagnosis = ''
+
+    # creating a button for Prediction
+    scaler_heart = pickle.load(open('C:/Users/Lenovo/OneDrive/Desktop/Multiple Disease Prediction System/saved_models/scaler_heart.pkl', 'rb'))
+    
+    if st.button('Heart Disease Test Result'):
+
+        input_data = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+       
+        input_data = [float(x) for x in input_data]
+        
+        input_data=scaler_heart.transform([input_data])
+        #print(input_data)
+        # change the input data to a numpy array
+        input_data_as_numpy_array= np.asarray(input_data)
+        #print(input_data)
+        #reshape the numpy array as we are predicting for only on instance
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+        
+        prediction = heart_disease_model.predict(input_data)
+        #print(prediction)
+        
+        if prediction[0]== 1:
+            heart_diagnosis = "The person has Heart Disease"
+        else:
+            heart_diagnosis = "The person does not have Heart Disease"
+
+        st.success(heart_diagnosis)
+
+# Parkinson's Prediction Page
+if selected == "Parkinsons Prediction":
+
+    # page title
+    st.title("Parkinson's Disease Prediction using ML")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+
+    with col5:
+        NHR = st.text_input('NHR')
+
+    with col1:
+        HNR = st.text_input('HNR')
+
+    with col2:
+        RPDE = st.text_input('RPDE')
+
+    with col3:
+        DFA = st.text_input('DFA')
+
+    with col4:
+        spread1 = st.text_input('spread1')
+
+    with col5:
+        spread2 = st.text_input('spread2')
+
+    with col1:
+        D2 = st.text_input('D2')
+
+    with col2:
+        PPE = st.text_input('PPE')
+
+    # code for Prediction
+    parkinsons_diagnosis = ''
+
+    # creating a button for Prediction    
+    if st.button("Parkinson's Test Result"):
+
+        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
+                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
+                      APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
+
+        user_input = [float(x) for x in user_input]
+
+        parkinsons_prediction = parkinsons_model.predict([user_input])
+
+        if parkinsons_prediction[0] == 1:
+            parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
+    st.success(parkinsons_diagnosis)
